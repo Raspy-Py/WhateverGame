aa686a1a209122ad2079551899a6543bbbbe4ff5
Roman Naumenko <naumenko.roman2003@gmail.com>
2024-02-09 18:01:56 +0200

    Update example.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,58 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <chrono>
+
+std::vector<char> Buffer(20 * 1024);
+
+void ReadData(asio::ip::tcp::socket& socket)
+{
+    socket.async_read_some(asio::buffer(Buffer.data(), Buffer.size()),
+                           [&](std::error_code ec, std::size_t length){
+        if (!ec)
+        {
+            std::cout << "\nRead " << length << "bytes\n\n";
+            for (int i = 0; i < length; ++i) {
+                std::cout << Buffer[i];
+            }
+
+            ReadData(socket);
+        }
+    });
+}
+
 
 int main()
 {
+    asio::error_code ec;
+    asio::io_context context;
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+    asio::ip::tcp::socket socket(context);
+    socket.connect(endpoint);
+
+    if(!ec)
+    {
+        std::cout << "Connected!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to connect! Error: " << ec.message() << std::endl;
+    }
+
+    std::string request = "GET /index.html HTTP/1.1\r\n"
+                          "Host: example.com\r\n"
+                          "Connection: close\r\n\r\n";
+
+    socket.write_some(asio::buffer(request.data(), request.size()), ec);
+    ReadData(socket);
+
+
     // Use the VideoMode object to create a Window
     auto vm = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(vm, "Window Title");
