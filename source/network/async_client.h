363589fcb9c6900fcb141172477e7723c531a604
RomanNaumenko <roman.naumenko@ucu.edu.us>
2024-02-22 12:14:38 +0200

    Added netwoork framework base files.

---
+++ source/network/async_client.h
@@ -0,0 +1,11 @@
+#ifndef WHATEVERGAME_SOURCE_NETWORK_CLIENT_H_
+#define WHATEVERGAME_SOURCE_NETWORK_CLIENT_H_
+
+#include <asio.hpp>
+
+template <typename T>
+class AsyncClient {
+  AsyncClient();
+};
+
+#endif //WHATEVERGAME_SOURCE_NETWORK_CLIENT_H_
---
+++ source/network/async_server.h
@@ -0,0 +1,10 @@
+#ifndef WHATEVERGAME_SOURCE_NETWORK_SERVER_H_
+#define WHATEVERGAME_SOURCE_NETWORK_SERVER_H_
+
+#include <asio.hpp>
+
+class AsyncServer {
+
+};
+
+#endif //WHATEVERGAME_SOURCE_NETWORK_SERVER_H_
